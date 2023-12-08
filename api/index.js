dd0f33a6e85e04dd39fbfea26a4633d7183e7324
vungocthy <ngocthyvu1107@gmail.com>
2023-12-08 21:06:20 +0700

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{ 
+    console.log("Connected to MongoDB");
+}).catch((e)=>{
+    console.log(e);
+});
+
+
 const app=express();
 
 app.listen(3000,()=>{
