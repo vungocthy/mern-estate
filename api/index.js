59ad02c3bec79bd54904c83dd68e7da52667b1cb
vungocthy <ngocthyvu1107@gmail.com>
2023-12-08 21:34:18 +0700

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,3 +17,9 @@ const app=express();
 app.listen(3000,()=>{
     console.log("Server run port 3000");
 });
+
+// app.get('/test',(req,res)=>{
+//     res.send('Hello world');
+// });
+
+app.use("/api/user",userRouter);
