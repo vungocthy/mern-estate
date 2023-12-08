866680dadb259d5afbe4cfb51f2e6e78ed71e6e6
vungocthy <ngocthyvu1107@gmail.com>
2023-12-08 23:27:14 +0700

    create signup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app=express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log("Server run port 3000");
 });
@@ -23,3 +26,4 @@ app.listen(3000,()=>{
 // });
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
