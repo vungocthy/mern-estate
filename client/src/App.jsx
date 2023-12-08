071c0cfb3182c9f18ca0c8dd208705a34362878d
vungocthy <ngocthyvu1107@gmail.com>
2023-12-08 09:51:35 +0700

    create page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 //import React from 'react'
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import Profile from './pages/Profile';
+import About from './pages/About';
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={Home}/>
+      <Route path='/sign-in' element={SignIn}/>
+      <Route path='/sign-up' element={SignUp}/>
+      <Route path='/profile' element={Profile}/>
+      <Route path='/about' element={About}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
