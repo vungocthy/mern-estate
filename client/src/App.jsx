2d1c135d9bfb64083b931436153bc13c161f70ee
vungocthy <ngocthyvu1107@gmail.com>
2023-12-09 03:14:23 +0700

    create sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 //import React from 'react'
 import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
@@ -12,8 +12,8 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/about' element={<About/>}/>
       </Routes>
