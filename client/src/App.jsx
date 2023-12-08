cc3071daa4601742df2c59b2c3b611421707a923
vungocthy <ngocthyvu1107@gmail.com>
2023-12-08 14:17:08 +0700

    nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
-      <Route path='/' element={Home}/>
-      <Route path='/sign-in' element={SignIn}/>
-      <Route path='/sign-up' element={SignUp}/>
-      <Route path='/profile' element={Profile}/>
-      <Route path='/about' element={About}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </BrowserRouter>
   )
