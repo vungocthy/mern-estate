32b639885c6d031ef89f5c2cb8ed19d8eda49de9
vungocthy <ngocthyvu1107@gmail.com>
2023-12-25 23:14:22 +0700

    add image slider

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -187,7 +187,7 @@ export default function Profile() {
           <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
           {userListing.map((listing)=>
           <div key={listing._id} className=" border rounded-lg p-3 flex justify-between items-center gap-4">
-            <Link to={`/listings/${listing._id}`}>
+            <Link to={`/listing/${listing._id}`}>
               <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain"/>
             </Link>
             <Link className="text-slate-700 font-semibold flex-1 hover:underline truncate" to={`/listings/${listing._id}`}>
